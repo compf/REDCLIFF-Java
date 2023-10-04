a5a043fdee45d6134e065d14fbca446e49c4d6ac
Nils <nilsbaumgartner1994@gmail.com>
2023-10-04 15:49:02 +0200

    Update

---
+++ scripts/runAction.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Launch IntelliJ IDEA with a custom parameter
+export TEST=true && /usr/local/bin/idea /Users/nbaumgartner/Documents/GitHub/REDCLIFF-Java
+
+# Optionally, use xdotool to simulate user interactions
+# xdotool search --name "IntelliJ IDEA" key ctrl+shift+A type "Your Action Name" key Return
+
