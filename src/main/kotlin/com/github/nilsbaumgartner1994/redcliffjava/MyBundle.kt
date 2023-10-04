288144c989fbfe4a9776ab257557c9d84ab48adb
GitHub Action <action@github.com>
2023-10-04 08:29:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/nilsbaumgartner1994/redcliffjava/MyBundle.kt
+++ src/main/kotlin/com/github/nilsbaumgartner1994/redcliffjava/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.nilsbaumgartner1994.redcliffjava
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/nilsbaumgartner1994/redcliffjava/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/nilsbaumgartner1994/redcliffjava/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nilsbaumgartner1994.redcliffjava.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
